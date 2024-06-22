577e23102e063c1bcb0a4410db65d653c63eb146
Gurjitnagra13 <gurinagra77@gmail.com>
2024-06-22 12:44:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @gurjit_nagra13
+# Subscribe YouTube Channel For Amazing Bot @gurjit.nagra13
+# Ask Doubt on telegram @gurjit_nagra13
 
 
 import re, os
